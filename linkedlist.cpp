049d24b9b53b8e9f1afd3c7850267af6cfcf0769
samadasyed <samadasyed@hotmail.com>
2025-11-07 19:08:47 -0500

    Added comments

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -26,3 +26,8 @@ public:
         return slow;
     }
 };
+
+//Given the head of a singly linked list, return the middle node of the linked list.
+//Input: head = [1,2,3,4,5]
+//Output: [3,4,5]
+//Explanation: The middle node of the list is node 3.
